556a4b30094e1577708529ea7aadbca4118e5707
Tufail Qurashe <tufail.javeed@gmail.com>
2016-10-04 15:39:58 +0530

    Updated Files

--- server.js
+++ server.js
@@ -1,21 +1,7 @@
 var express = require('express');
 var app =  express();
 var PORT=3000; 
-
-var middleware = {
-	requireAuthentication : function (req, res, next){
-		console.log('private route hit!')
-		next();
-	},
-	logger:function(req,res,next) {
-		console.log('Request on '+new Date().toString()+':'+req.method+' '+req.originalUrl);
-		next();
-	}
-};
-
-//order of definition is important
-//adding application level middleware
-//app.use(middleware.requireAuthentication);
+var middleware =  require('./middleware.js');
 
 app.use(middleware.logger);
 
